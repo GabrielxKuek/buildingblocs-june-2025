85088446037ed94a3db50a64f3fb9a7356f4cd3f
GabrielxKuek <kuekguanjiegabriel@gmail.com>
2025-06-11 04:34:05 +0800

    update routes to handle for mystery endpoint

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,21 +6,34 @@ import TestPage from './pages/TestPage'
 import ErrorPage from './pages/ErrorPage'
 import CaregiverPage from './pages/CaregiverPage'
 import PatientPage from './pages/PatientPage'
-import Navbar from "@/components/misc/Navbar"
+import Navbar from "@/components/system/Navbar"
+import Sidebar from "@/components/system/Sidebar"
 
 function App() {
   return (
     <BrowserRouter>
-      {/* Navbar */}
-      <Navbar/>
+      <div className="h-screen flex flex-col">
+        {/* navbar */}
+        <Navbar/>
 
-      {/* Routes */}
-      <Routes>
-        <Route path="/test" element={<TestPage />} />
-        <Route path="/caregiver" element={<CaregiverPage />} />
-        <Route path="/patient" element={<PatientPage />} />
-        <Route path="*" element={<ErrorPage />} />
-      </Routes>
+        {/* main layout */}
+        <div className="flex flex-1 overflow-hidden">
+          {/* sidebar */}
+          <Sidebar />
+          
+          {/* content */}
+          <main className="flex-1 overflow-auto p-6">
+            <Routes>
+              <Route path="/test" element={<TestPage />} />
+              <Route path="/caregiver" element={<CaregiverPage />} />
+              <Route path="/caregiver/*" element={<CaregiverPage />} />
+              <Route path="/patient" element={<PatientPage />} />
+              <Route path="/patient/*" element={<PatientPage />} />
+              <Route path="*" element={<ErrorPage />} />
+            </Routes>
+          </main>
+        </div>
+      </div>
     </BrowserRouter>
   );
 }
