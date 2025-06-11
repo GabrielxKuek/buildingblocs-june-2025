0bbd6d3141ed1884813830d71b94859df0d69033
ProjektKevin <minthetkhine05@.gmail.com>
2025-06-12 02:04:07 +0800

    Fix(): Fixed the nav bar padding

--- client/src/components/system/Navbar.jsx
+++ client/src/components/system/Navbar.jsx
@@ -15,7 +15,7 @@ const Navbar = () => {
     };
     
     return (
-        <div className="flex justify-end p-8 border-b">
+        <div className="flex justify-end px-4 py-2 border-b">
             <ToggleGroup 
                 type="single" 
                 value={getCurrentValue()}
